0a9ec9129f1fec0b845170196fc4c70c1c9d8e47
Simone Karin Lehmann <simone@lisanet.de>
2020-01-05 00:34:03 +0100

    ensure that cached accessories are not added again. slightly change logging, reformat some lines

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ module.exports = function(homebridge) {
 // config may be null
 // api may be null if launched from old homebridge version
 function ProwlNotification(log, config, api) {
-    log("ProwlNotification Init");
     this.log = log;
     this.config = config;
     this.accessories = [];
@@ -31,6 +30,12 @@ function ProwlNotification(log, config, api) {
 ProwlNotification.prototype.didFinishLaunching = function () {
     // Add or update accessories defined in config.json
     for (var i in this.switches) this.addAccessory(this.switches[i]);
+    
+    // Remove extra accessories in cache
+    for (var name in this.accessories) {
+      var accessory = this.accessories[name];
+      if (!accessory.reachable) this.removeAccessory(accessory);
+    }
 }
 
 
@@ -38,7 +43,7 @@ ProwlNotification.prototype.didFinishLaunching = function () {
 // Developer can configure accessory at here (like setup event handler).
 // Update current value.
 ProwlNotification.prototype.configureAccessory = function(accessory) {
-    this.log("Configure Accessory");
+    this.log("Configure cached accessory:", accessory.displayName);
     
     this.setService(accessory);
     this.accessories[accessory.context.name] = accessory;
@@ -49,19 +54,22 @@ ProwlNotification.prototype.configureAccessory = function(accessory) {
 
 // we get accessory data from config.json
 ProwlNotification.prototype.addAccessory = function(data) {
-    this.log("Add Accessory");
-    var uuid;
+    var accessory = this.accessories[data.name];
     
-    
-    
-    // Setup accessory as SWITCH (8) category.
-    var uuid = UUIDGen.generate(data.name);
-    accessory = new Accessory(data.name, uuid, 8);
-    // Setup HomeKit switch service
-    accessory.addService(Service.Switch, data.name);
-    // New accessory is always reachable
-    accessory.reachable = true;
-    
+    if (!accessory) {
+        this.log("Add new accessory:", data.name);
+        // Setup accessory as SWITCH (8) category.
+        var uuid = UUIDGen.generate(data.name);
+        accessory = new Accessory(data.name, uuid, 8);
+        // Setup HomeKit switch service
+        accessory.addService(Service.Switch, data.name);
+        // New accessory is always reachable
+        accessory.reachable = true;
+        // Setup listeners for different switch events
+        this.setService(accessory);
+        // Register new accessory in HomeKit
+        this.api.registerPlatformAccessories("homebridge-prowl-notification", "ProwlNotification", [accessory]);
+    }
     /*
      name: name in Homekit
      subject: subject of  push notification, default is room name
@@ -85,15 +93,10 @@ ProwlNotification.prototype.addAccessory = function(data) {
     .setCharacteristic(Characteristic.Model, model)
     .setCharacteristic(Characteristic.SerialNumber, serial);
     
-    
-    // Setup listeners for different switch events
-    this.setService(accessory);
-    
     // Store accessory in cache
     this.accessories[data.name] = accessory;
-    // Register new accessory in HomeKit
-    this.api.registerPlatformAccessories("homebridge-samplePlatform", "ProwlNotification", [accessory]);
-    
+    // Configured accessory is reachable
+    accessory.updateReachability(true);
 }
 
 
@@ -113,16 +116,21 @@ ProwlNotification.prototype.identify = function (thisSwitch, paired, callback) {
     callback();
 }
 
+// Method to remove accessories from HomeKit
+ProwlNotification.prototype.removeAccessory = function (accessory) {
+  if (accessory) {
+    var name = accessory.context.name;
+    this.api.unregisterPlatformAccessories("homebridge-prowl-notification", "ProwlNotification", [accessory]);
+    delete this.accessories[name];
+  }
+}
 
 ProwlNotification.prototype.getState = function (thisSwitch, callback) {
-    var self = this;
     callback(null);
 }
 
 ProwlNotification.prototype.setState = function (thisSwitch, state, callback) {
     var self = this;
-    
-    this.log("setState");
     
     if (state == true) {
         
@@ -137,7 +145,7 @@ ProwlNotification.prototype.setState = function (thisSwitch, state, callback) {
 }
 
 ProwlNotification.prototype.sendNotification = function(thisSwitch) {
-    this.log("send notification");
+    this.log.debug("send notification from " + thisSwitch.name);
     
     var data = querystring.stringify({ apikey : this.config.apikey,
                                      application : thisSwitch.subject,
@@ -157,11 +165,11 @@ ProwlNotification.prototype.sendNotification = function(thisSwitch) {
     };
     
     var req = https.request(options, res => {
-                            this.log(`sendNotification statusCode: ${res.statusCode}`)
+                            this.log.debug(`sendNotification statusCode: ${res.statusCode}`)
                             })
     
     req.on('error', error => {
-           this.log("senNotification" + error)
+           this.log.error("sendNotification: " + error)
            })
     
     req.write(data)
